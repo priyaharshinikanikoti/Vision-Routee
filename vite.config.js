03e27af73dd75a06ce4260e584a15bf83077b858
priyaharshinikanikoti <priyaharshinikanikoti@gmail.com>
2026-09-11 09:21:35 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Vision-Routee/',
   server: {
     port: 3000,
-    host: '0.0.0.0', // Bind to all network interfaces for local and loopback access
+    host: '0.0.0.0',
     strictPort: false
   }
 });
